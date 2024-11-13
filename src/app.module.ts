0858e90c61bdc4ffbb23e0e6588808cb09a18931
adkshishir <adhikarishishir50@gmail.com>
2024-11-13 15:30:28 +0545

    feat: init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MailerService } from './mail/mail.service';
+import { BookingModule } from './booking/booking.module';
+import { PackagesModule } from './packages/packages.module';
+import { PilotModule } from './pilot/pilot.module';
+
+@Module({
+  imports: [AuthModule, PrismaModule, BookingModule, PackagesModule, PilotModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService, MailerService],
+})
+export class AppModule {}
