ecf3b0de311a8df466987b1d67ca21717a918644
adkshishir <adhikarishishir50@gmail.com>
2024-11-15 16:50:24 +0545

    refactor: booking dto

--- src/booking/dto/create-booking.dto.ts
+++ src/booking/dto/create-booking.dto.ts
@@ -15,13 +15,17 @@ enum Method {
   ONLINE = 'ONLINE',
 }
 
-enum FlightType {
-  COMMERICAL = 'COMMERICAL',
-  FREE = 'FREE',
-  TRAINING = 'TRAINING',
-  TEST = 'TEST',
-}
+// enum FlightType {
+//   COMMERICAL = 'COMMERICAL',
+//   FREE = 'FREE',
+//   TRAINING = 'TRAINING',
+//   TEST = 'TEST',
+// }
 
+enum CraftType {
+  OPEN = 'OPEN',
+  CLOSE = 'CLOSE',
+}
 export class CreateBookingDto {
   @ApiProperty({ example: '2023-01-01', required: true })
   @IsString({ message: 'Flight Date is required' })
@@ -31,16 +35,19 @@ export class CreateBookingDto {
   nationality: string;
   @ApiProperty({ example: 200, required: false })
   @IsNumber()
+  @IsOptional()
   prePayment: number;
   @ApiProperty({ example: 100, required: false })
   @IsNumber()
+  @IsOptional()
   discount: number;
   @ApiProperty({ example: 'COMMERICAL', required: true })
-  @IsEnum(FlightType)
-  flightType: FlightType;
-
+  @IsString({ message: 'Flight Type Must be COMMERICAL|FREE|TRAINING|TEST' })
+  @IsOptional()
+  flightType: string;
   @ApiProperty({ example: 'CASH', required: true })
   @IsEnum(Method)
+  @IsOptional()
   paymentMethod: Method;
 
   @ApiProperty({ example: 1, required: true })
@@ -48,15 +55,16 @@ export class CreateBookingDto {
   packageId: number;
 
   @ApiProperty()
+  @IsOptional()
   @IsNumber()
   pilotId: number;
   @ApiProperty({ example: false, required: false })
   @IsBoolean()
   includes: boolean;
-  @ApiProperty({ example: 100, required: false })
-  @IsNumber()
-  @IsOptional()
-  commission: number;
+  // @ApiProperty({ example: 100, required: false })
+  // @IsNumber()
+  // @IsOptional()
+  // commission: number;
   @IsString()
   @ApiProperty({ example: 'name', required: false })
   pName: string;
@@ -65,8 +73,13 @@ export class CreateBookingDto {
   pId: string;
   @ApiProperty()
   @IsString()
+  @IsOptional()
   ticketNo: string;
   @ApiProperty()
   @IsString({ message: 'Identification Type is required' })
-  identificationType: string;
+  pIdType: string;
+  @ApiProperty()
+  @IsEnum(CraftType) // This will validate the enum value
+  @IsOptional()
+  aircraftType: CraftType;
 }
