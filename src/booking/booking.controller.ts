05190cdb61e1ef2c68d92e006240f985301899ce
adkshishir <adhikarishishir50@gmail.com>
2024-11-20 22:26:14 +0545

    feat: update status and commisssion calculation

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -10,14 +10,14 @@ import {
   Query,
 } from '@nestjs/common';
 import { BookingService } from './booking.service';
-import { CreateBookingDto, PaginationQueryDto } from './dto/create-booking.dto';
+import { CreateBookingDto, PaginationQueryDto, UpdateInAirportDto, updateStatusDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CurrentUser } from 'src/current-user/current-user.decorator';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { Roles } from 'src/roles/roles.decorator';
 import { RolesGuard } from 'src/roles/roles.guard';
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
@@ -46,12 +46,35 @@ export class BookingController {
 
   @Patch(':id')
   @ApiBearerAuth()
+  @Roles('ADMIN')
   update(
     @Param('id') id: string,
     @CurrentUser() user: any,
     @Body() updateBookingDto: UpdateBookingDto,
   ) {
     return this.bookingService.update(+id, updateBookingDto, user);
+  }
+  @Patch('status/:id')
+  @ApiBearerAuth()
+  @Roles('ADMIN')
+  updateStatus(
+    @Param('id') id: string,
+    @CurrentUser() user: any,
+    @Body() updateStatusDto: updateStatusDto,
+  ) {
+    return this.bookingService.updateStatus(+id, updateStatusDto, user);
+  }
+
+  @ApiBearerAuth()
+  @Patch('airport/:id')
+  @Roles('ADMIN', 'AIRPORT')
+  updateInAirport(
+    @Param('id') id: string,
+    @CurrentUser() user: any,
+    @Body()
+    updateInAirportDto:UpdateInAirportDto,
+  ) {
+    return this.bookingService.updateInAirport(+id, updateInAirportDto, user);
   }
 
   @Delete(':id')
@@ -68,3 +91,4 @@ export class BookingController {
     return this.bookingService.removeAll();
   }
 }
+
