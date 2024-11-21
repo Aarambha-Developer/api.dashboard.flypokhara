19585c5d9b9ee74405b7cc4379fd855d0f29880d
adkshishir <adhikarishishir50@gmail.com>
2024-11-21 16:47:27 +0545

    refactor: mail name service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -16,13 +16,13 @@ export class MailerService {
   async sendForgotPasswordEmail(to: string, resetToken: string) {
     const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
     await this.transporter.sendMail({
-      from: '"Your App" <no-reply@yourapp.com>',
+      from: "'Flypokhara' <" + process.env.MAIL_FROM + '>',
       to,
       subject: 'Password Reset Request',
       html: ` <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
           <h2 style="color: #333;">Password Reset Request</h2>
           <a href="${`${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`}">Reset Password</a> <br/>
-          <p>You requested a password reset. Click the button below to reset your password:</p>
+          <p>You requested a password reset. Click the button Above to reset your password:</p>
           <p style="margin-top: 20px;">If you did not request this, please ignore this email.</p>
           <p style="color: #555;">This link is valid for 15 minutes.</p>
         </div>`,
